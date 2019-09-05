0b6e95c588f07cda58585c5d5032cd1b474bf1b5
GKCB <hmkscbo@sina.com>
2019-09-05 13:55:49 +0800

    week01

---
+++ codes/Python-S14/week01/var.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   var.py
+@Time    :   2019/09/05 13:46:24
+@Author  :   CHENG Bo 
+@Version :   1.0
+@Contact :   hmkscbo@sina.com
+@Github  :   https://github.com/GKCB
+@Desc    :   None
+'''
+name = "CHENG Bo"
+print("My name is ",name)
+
---
+++ codes/Python-S14/week01/var2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   var.py
+@Time    :   2019/09/05 13:46:24
+@Author  :   CHENG Bo 
+@Version :   1.0
+@Contact :   hmkscbo@sina.com
+@Github  :   https://github.com/GKCB
+@Desc    :   None
+'''
+name = "CHENG Bo"
+name2 = name
+print("My name is ",name,name2)
+name = "Louis"
+print("My name is ",name,name2)
+
+
