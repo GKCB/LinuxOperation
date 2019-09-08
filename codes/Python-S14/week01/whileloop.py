1b3cd6171cb4fea507330b77f19d4241b7273994
GKCB <hmkscbo@sina.com>
2019-09-08 23:31:04 +0800

    week01

---
+++ codes/Python-S14/week01/whileloop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   whileloop.py
+@Time    :   2019/09/08 16:01:41
+@Author  :   CHENG Bo 
+@Version :   1.0
+@Contact :   hmkscbo@sina.com
+@Github  :   https://github.com/GKCB
+@Desc    :   None
+'''
+count = 0
+while True:
+    print("count",count)
+    count += 1
+
+
